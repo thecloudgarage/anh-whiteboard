6d05020118a72f9eb10adfe9244a80a71fa03eb2
root <root@ip-172-31-52-159.ec2.internal>
2020-09-02 17:39:18 +0000

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var app = require('express')();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 var path = require('path');
 var bodyParser = require('body-parser'); // for reading POSTed form data into `req.body`
 
